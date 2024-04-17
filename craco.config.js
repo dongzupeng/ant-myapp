bc8916551ee15450a1daeb0e91951ecfedd9e438
dzp <2360591410@qq.com>
2024-04-18 01:55:19 +0800

    test

--- craco.config.js
+++ craco.config.js
@@ -32,8 +32,7 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
-            outputPath: 'static/media',
-            publicPath: '/ant-myapp/static/media',
+            publicPath: '/ant-myapp/',
           },
         });
       }
